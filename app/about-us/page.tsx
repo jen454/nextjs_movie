364b374b25e63c00137f90e3240f73c6734e5361
Jinwook <stukids2765@gmail.com>
2024-02-13 16:07:22 +0900

    next14_study

---
+++ app/about-us/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata :Metadata = {
+    title: "About us",
+}
+
+export default function AboutUs() {
+    return (
+        <div>
+            <h1>About us!</h1>
+        </div>
+    );
+}
